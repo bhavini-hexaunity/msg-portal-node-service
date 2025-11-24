69b887d95e0d6d1cb2b82092559a9a5f298a0466
bhavini <bhavini@hexaunity.com>
2025-11-24 09:27:37 +0530

    Add prisma

--- src/services/restaurants.service.js
+++ src/services/restaurants.service.js
@@ -1,21 +1,49 @@
-const repo = require("../repositories/restaurants.repo");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = {
-  list: () => repo.findAll(),
+  // Get all restaurants
+  findAll: async () => {
+    return await prisma.restaurant.findMany();
+  },
 
-  get: (id) => repo.findById(id),
+  // Get one restaurant by ID
+  findById: async (id) => {
+    return await prisma.restaurant.findUnique({
+      where: { id },
+    });
+  },
 
+  // Create a new restaurant
   create: async (name) => {
-    const created = await repo.create(name);
-    return created;
+    const id = uuidv4();
+
+    const restaurant = await prisma.restaurant.create({
+      data: {
+        id,
+        name,
+      },
+    });
+
+    return restaurant;
   },
 
+  // Update by ID
   update: async (id, name) => {
-    const updated = await repo.update(id, name);
-    return updated;
+    const restaurant = await prisma.restaurant.update({
+      where: { id },
+      data: { name },
+    });
+
+    return restaurant;
   },
 
+  // Delete by ID
   delete: async (id) => {
-    await repo.delete(id);
-  }
+    await prisma.restaurant.delete({
+      where: { id },
+    });
+    return { message: "Deleted successfully", id };
+  },
 };
