f868bd2892eb711f4eb240e34d76152565077767
bhavini <bhavini@hexaunity.com>
2025-11-24 14:29:27 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express,{Request,Response,NextFunction, Application} from 'express';
+import express, { Request, Response, NextFunction, Application } from 'express';
 import cors from "cors";
 import morgan from "morgan";
 import restaurantRoutes from "./routes/restaurants.routes"
@@ -13,6 +13,18 @@ app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev")); // Logs all requests in dev mode
+
+// ------------------------
+// Sheet API
+// ------------------------
+app.post("/api/sheet-sync", (req, res) => {
+  console.log("Received data:", req.body);
+  res.status(200).json({
+    success: true,
+    message: "Data received",
+    received: req.body,
+  });
+});
 
 // ------------------------
 // Health Check Route
