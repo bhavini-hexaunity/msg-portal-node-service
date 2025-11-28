26339e0bfa3066bba24ffd3f5d4f41a6c9853efa
bhavini <bhavini@hexaunity.com>
2025-11-28 15:29:50 +0530

    fixed prisma issue

--- src/controllers/sync.controller.ts
+++ src/controllers/sync.controller.ts
@@ -2,19 +2,16 @@ import { RequestHandler } from "express";
 import { syncDepositService, syncProfitService, syncToplineService } from "../services/sync.service";
 
 export const handleToplineSync: RequestHandler = async (req, res) => {
-  console.log("🔥 Topline Payload Received:", req.body);
   const result = await syncToplineService.upsert(req.body);
   res.json({ success: true, data: result });
 };
 
 export const handleProfitSync: RequestHandler = async (req, res) => {
-  console.log("📘 Profit Payload Received:", req.body);
   const result = await syncProfitService.upsert(req.body);
   res.json({ success: true, data: result });
 };
 
 export const handleDepositSync: RequestHandler = async (req, res) => {
-  console.log("💰 Deposit Payload Received:", req.body);
   const result = await syncDepositService.upsert(req.body);
   res.json({ success: true, data: result });
 };
